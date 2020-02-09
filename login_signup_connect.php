77260046aa7e55e405573bfcaaa1d289aa902173
ankit039 <ryder.raj.ankit@gmail.com>
2020-02-10 00:51:44 +0530

    login signup connect

---
+++ login_signup_connect.php
@@ -0,0 +1,153 @@
+<?php
+session_start();
+include_once 'db.php';
+
+$var= 0;
+if(isset($_POST['login_submit']))
+{
+	$hname = mysqli_real_escape_string($conn, $_POST['hname']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	$var= 1;
+}
+if(isset($_POST['signup_submit']))
+{
+	$hname = mysqli_real_escape_string($conn, $_POST['hname']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+	$cpwd = mysqli_real_escape_string($conn, $_POST['cpwd']);
+	$var= 2;
+}
+//login
+if($var === 1)
+{
+if(empty($hname) || empty($password))
+{
+		header("location: login_signup?login=empty");
+		exit();
+	}
+		else
+		{
+
+		$sql="SELECT * FROM logindetail WHERE hname ='$hname'";
+		$result= mysqli_query($conn, $sql);
+		$resultcheck = mysqli_num_rows($result);
+
+		if($resultcheck < 1)
+		{
+			header("location: login_signup?login=error_wrong_username");
+			exit();
+		}
+	
+	   else
+	   {
+	   	$row = mysqli_fetch_assoc($result);
+			if (count($row)) 
+		{
+	   	if ($row["status"]=='Unverified')
+	   	{
+	   		//echo"<script>alert('Please Contact Founders and IC's');</script>";
+			header("location: login_signup?login=status_Unverified_account_or_Banned");
+			exit();
+		}
+		else
+		{
+				if($password === $row['password'])
+				{ 
+				   $sendAdminPanel = array(13,14);
+				   $sendSeniorPanel = array(7,8,9,10,11,12);
+				   $sendJuniorPanel = array(0,1,2,3,4,5,6);
+				if(in_array($row['rank_code'],  $sendAdminPanel))
+				  {
+				  	//admin panel
+					header("location:home_1");
+					$_SESSION["hname"] = "$hname";
+					exit();
+				  }
+				  elseif(in_array($row['rank_code'],  $sendSeniorPanel))
+				  {
+				  	//senior panel
+					header("location:home_2");
+					$_SESSION["hname"] = "$hname";
+					exit();
+				  }
+				  elseif(in_array($row['rank_code'],  $sendJuniorPanel))
+				  {
+				  	//junior panel
+					header("location:home_3");
+					$_SESSION["hname"] = "$hname";
+					exit();
+				  }
+				}
+
+				else
+				{
+					header("location: login_signup?login=error_wrong_password");
+						exit();
+				}
+			
+	        }
+	    }
+}
+}
+}
+//signup
+elseif($var === 2)
+{
+	if (empty($hname)|| empty($email)||empty($pwd)|| empty($cpwd)) 
+	{
+		header("location: login_signup?signup=empty");
+		exit();
+	}
+	else
+	{
+			if (!filter_var($email,FILTER_VALIDATE_EMAIL)) 
+			{
+			header("location: login_signup?signup=notvalid_email");
+			exit();
+			}
+			else if(($pwd)!=($cpwd))
+			{
+			header("location: login_signup?signup=password_not_match");
+			exit();
+			}
+				$sql = "SELECT * FROM logindetail WHERE hname='$hname'";
+				$result = mysqli_query($conn,$sql);
+				$resultcheck = mysqli_num_rows($result);
+
+				if($resultcheck > 0)
+				{
+					header("location: login_signup?signup=habbo_username_taken");
+					exit();
+				} 
+                   
+                    $status='Unverified';
+                    $date=date("d-m-Y h:ia");
+           			$row = mysqli_fetch_assoc($result);
+					$sql= "INSERT INTO logindetail (hname,email,password,status,rank_code,ip,signupdate) VALUES 
+					('$hname','$email','$pwd','$status','0','$store_ip','$date');";
+					mysqli_query($conn,$sql);
+					$sql= "INSERT INTO profiledetail (hname,email) VALUES 
+					('$hname','$email');";
+					mysqli_query($conn,$sql);
+					$sql= "INSERT INTO promotion (hname,oldrank,newrank,updatedby,updatedate) VALUES 
+					('$hname','N/A','Recruit','ADMIN','$date');";
+					mysqli_query($conn,$sql);
+					$sql= "INSERT INTO demotion (hname,oldrank,newrank,updatedby,updatedate) VALUES 
+					('$hname','N/A','N/A','ADMIN','$date');";
+					mysqli_query($conn,$sql);
+					
+			if($hname  && $email && $pwd && $status && $store_ip && $date) 
+			{
+            header('location: login_signup?signup=Successfull_wait_to_get_verified');
+            } 
+							exit();
+				
+	}
+}
+else
+{
+	//not valid login or signup
+	header('location: login_signup?signup=you_cannot_access_this_page');
+	exit();
+}
+?>
