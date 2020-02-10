15663e4c492cedcdb806b44171db7155fb7a8bf0
ankit039 <ryder.raj.ankit@gmail.com>
2020-02-11 02:36:23 +0530

    normal profile connect

--- n_profile_connect.php
+++ n_profile_connect.php
@@ -1,13 +1,9 @@
 <?php
-
-$dbname = "localhost";
-$username = "root";
-$password = "";
-$database = "portal_habbo_agency";
-
-$mysqli = new mysqli($dbname, $username, $password, $database);
+include_once 'db2.php';
 
 $query_profile = "SELECT * FROM profiledetail where hname='$habboname'";
+$query_pro = "SELECT * FROM rankactivity where hname='$habboname'";
+$query_status = "SELECT status FROM logindetail where hname='$habboname'";
 if ($checkrun = mysqli_num_rows(mysqli_query($mysqli,$query_profile))>0) {
   $new=0;
 } else {
