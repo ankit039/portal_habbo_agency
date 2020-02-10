eea3f4b76a4856d32d749bb49d369384e805a1a6
ankit039 <ryder.raj.ankit@gmail.com>
2020-02-11 02:35:40 +0530

    login signup connect

--- login_signup_connect.php
+++ login_signup_connect.php
@@ -22,6 +22,7 @@ if($var === 1)
 {
 if(empty($hname) || empty($password))
 {
+	    $sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login empty');"; mysqli_query($conn,$sql);
 		header("location: login_signup?login=empty");
 		exit();
 	}
@@ -34,6 +35,7 @@ if(empty($hname) || empty($password))
 
 		if($resultcheck < 1)
 		{
+			$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login wrong username');"; mysqli_query($conn,$sql);
 			header("location: login_signup?login=error_wrong_username");
 			exit();
 		}
@@ -45,7 +47,7 @@ if(empty($hname) || empty($password))
 		{
 	   	if ($row["status"]=='Unverified')
 	   	{
-	   		//echo"<script>alert('Please Contact Founders and IC's');</script>";
+	   		$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login banned or unverified');"; mysqli_query($conn,$sql);
 			header("location: login_signup?login=status_Unverified_account_or_Banned");
 			exit();
 		}
@@ -56,31 +58,47 @@ if(empty($hname) || empty($password))
 				   $sendAdminPanel = array(13,14);
 				   $sendSeniorPanel = array(7,8,9,10,11,12);
 				   $sendJuniorPanel = array(0,1,2,3,4,5,6);
+				   $notallowed = array('-1');
 				if(in_array($row['rank_code'],  $sendAdminPanel))
 				  {
 				  	//admin panel
+				  	$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login admin');"; mysqli_query($conn,$sql);
 					header("location:home_1");
 					$_SESSION["hname"] = "$hname";
+					$_SESSION["rank_code1"] = "admin";
 					exit();
 				  }
 				  elseif(in_array($row['rank_code'],  $sendSeniorPanel))
 				  {
 				  	//senior panel
+				  	$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login senior');"; mysqli_query($conn,$sql);
 					header("location:home_2");
 					$_SESSION["hname"] = "$hname";
+					$_SESSION["rank_code2"] = "senior";
 					exit();
 				  }
 				  elseif(in_array($row['rank_code'],  $sendJuniorPanel))
 				  {
 				  	//junior panel
+				  	$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login junior');"; mysqli_query($conn,$sql);
 					header("location:home_3");
 					$_SESSION["hname"] = "$hname";
+					$_SESSION["rank_code3"] = "junior";
+					$_SESSION["check"] = 1;
+					exit();
+				  }
+				  elseif(in_array($row['rank_code'],  $notallowed))
+				  {
+				  	//not allowed panel
+				  	$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login banned or unverified');"; mysqli_query($conn,$sql);
+					header("location: login_signup?login=status_Unverified_account_or_Banned");
 					exit();
 				  }
 				}
 
 				else
 				{
+					$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','login wrong password');"; mysqli_query($conn,$sql);
 					header("location: login_signup?login=error_wrong_password");
 						exit();
 				}
@@ -95,6 +113,7 @@ elseif($var === 2)
 {
 	if (empty($hname)|| empty($email)||empty($pwd)|| empty($cpwd)) 
 	{
+		$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','signup empty');"; mysqli_query($conn,$sql);
 		header("location: login_signup?signup=empty");
 		exit();
 	}
@@ -102,11 +121,13 @@ elseif($var === 2)
 	{
 			if (!filter_var($email,FILTER_VALIDATE_EMAIL)) 
 			{
+			$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','signup not valid mail');"; mysqli_query($conn,$sql);
 			header("location: login_signup?signup=notvalid_email");
 			exit();
 			}
 			else if(($pwd)!=($cpwd))
 			{
+			$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','signup bot password not matched');"; mysqli_query($conn,$sql);
 			header("location: login_signup?signup=password_not_match");
 			exit();
 			}
@@ -116,6 +137,7 @@ elseif($var === 2)
 
 				if($resultcheck > 0)
 				{
+					$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','signup but username taken');"; mysqli_query($conn,$sql);
 					header("location: login_signup?signup=habbo_username_taken");
 					exit();
 				} 
@@ -124,20 +146,18 @@ elseif($var === 2)
                     $date=date("d-m-Y h:ia");
            			$row = mysqli_fetch_assoc($result);
 					$sql= "INSERT INTO logindetail (hname,email,password,status,rank_code,ip,signupdate) VALUES 
-					('$hname','$email','$pwd','$status','0','$store_ip','$date');";
+					('$hname','$email','$pwd','$status','-1','$store_ip','$date');";
 					mysqli_query($conn,$sql);
 					$sql= "INSERT INTO profiledetail (hname,email) VALUES 
 					('$hname','$email');";
 					mysqli_query($conn,$sql);
-					$sql= "INSERT INTO promotion (hname,oldrank,newrank,updatedby,updatedate) VALUES 
-					('$hname','N/A','Recruit','ADMIN','$date');";
+					$sql= "INSERT INTO rankactivity (hname,oldrank,newrank,updatedby,updatedate,type) VALUES 
+					('$hname','N/A','Recruit','ADMIN','$date','1');";
 					mysqli_query($conn,$sql);
-					$sql= "INSERT INTO demotion (hname,oldrank,newrank,updatedby,updatedate) VALUES 
-					('$hname','N/A','N/A','ADMIN','$date');";
-					mysqli_query($conn,$sql);
-					
+
 			if($hname  && $email && $pwd && $status && $store_ip && $date) 
 			{
+			$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','signup sucess');"; mysqli_query($conn,$sql);
             header('location: login_signup?signup=Successfull_wait_to_get_verified');
             } 
 							exit();
@@ -147,6 +167,7 @@ elseif($var === 2)
 else
 {
 	//not valid login or signup
+	$sql= "INSERT INTO iptrack (hname,type,ip,reason) VALUES ('$hname','$var','$store_ip','unauthorised access');"; mysqli_query($conn,$sql);
 	header('location: login_signup?signup=you_cannot_access_this_page');
 	exit();
 }
